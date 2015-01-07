33d3793703a8e4ee251dbfed29c8c8233445944c
Carlos Fernández <sirikon2@gmail.com>
2015-01-07 12:14:33 +0100

    Finalizado Ejercicio 5

--- R6E5/R6E5/Program.cs
+++ R6E5/R6E5/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace R6E5
@@ -9,7 +8,35 @@ namespace R6E5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(contarPalabras(Console.ReadLine(), Console.ReadLine()[0]));
+            Console.WriteLine("Este programa contará las palabras de una frase");
+            Console.Write("Frase: ");
+            String phrase = Console.ReadLine();
+            Console.Write("Separador: ");
+            String separator = Console.ReadLine();
+
+            if (phrase == "")
+            {
+                Console.WriteLine("No has introducido ninguna frase"); Salir(); return;
+            }
+
+            if (separator.Length == 0)
+            {
+                Console.WriteLine("No has introducido ningún separador"); Salir(); return;
+            }
+
+            Int32 cPalabras = contarPalabras(removeRepeatChar(phrase,separator[0]), separator[0]);
+            String palabras = "palabras";
+            if (cPalabras == 1)
+                palabras = "palabra";
+
+            Console.WriteLine("La frase tiene " + cPalabras + " " + palabras);
+            Salir();
+        }
+
+        static void Salir()
+        {
+            Console.WriteLine("Pulse enter para salir...");
+            Console.ReadLine();
         }
 
         static Int32 contarPalabras(String t, Char s)
@@ -25,6 +52,21 @@ namespace R6E5
                 }
             }
             return c;
+        }
+
+        static String removeRepeatChar(String t, Char c)
+        {
+            StringBuilder r = new StringBuilder();
+            Char lastChar = new Char();
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] != lastChar)
+                {
+                    r.Append(t[i]);
+                }
+                lastChar = t[i];
+            }
+            return r.ToString();
         }
     }
 }
