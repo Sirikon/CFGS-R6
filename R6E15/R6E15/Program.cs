ac91ba9ecbd86a971906c0bcc3a66fe5d12132ab
Carlos Fernández <sirikon2@gmail.com>
2015-01-14 12:45:18 +0100

    Empezado ejercicio 15

---
+++ R6E15/R6E15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace R6E15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu("Gestor de ArrayList", "Selecciona una de las opciones", null);
+        }
+
+        static Int32 Menu(String title, String subtitle, String[] options)
+        {
+            Console.Clear();
+            Console.WriteLine("\r\n   " + title);
+            Console.WriteLine(" ".PadRight(title.Length + 5,'='));
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(" " + subtitle + "\r\n");
+            Console.ResetColor();
+
+            
+
+            return 0;
+        }
+    }
+}
